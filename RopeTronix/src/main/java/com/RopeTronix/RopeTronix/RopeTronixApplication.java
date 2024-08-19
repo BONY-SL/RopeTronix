dade30129b042eb350b6cd7abcd38c404653e310
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-08-19 22:29:10 +0530

    create project

---
+++ RopeTronix/src/main/java/com/RopeTronix/RopeTronix/RopeTronixApplication.java
@@ -0,0 +1,13 @@
+package com.RopeTronix.RopeTronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RopeTronixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RopeTronixApplication.class, args);
+	}
+
+}
---
+++ RopeTronix/src/test/java/com/RopeTronix/RopeTronix/RopeTronixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RopeTronix.RopeTronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RopeTronixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
